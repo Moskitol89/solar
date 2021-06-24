dd8dd42624f0203739e938ceb63a51a25431eb28
Moskitol89 <kurganovv@yahoo.com>
2021-06-24 17:46:52 +0300

    rate block started

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".rate__info-slider", {
+    autoplay: true,
+    loop: true,
+    navigation: {
+        nextEl: ".rate__info-arrow-next",
+        prevEl: ".rate__info-arrow-prev"
+    }
+
+});
