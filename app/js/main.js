5ffda99f06f4afaf90221f6da5ebc7776b0585ae
Moskitol89 <kurganovv@yahoo.com>
2021-06-25 17:04:23 +0300

    rate tabs done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,30 @@
+
+
+const tabButtons = Array.prototype.slice
+    .call(document.querySelector(".rate__tabs-names")
+    .children),
+    tabs = Array.prototype.slice
+        .call(document.querySelector(".rate__tabs")
+            .children);
+
+document.querySelectorAll(".rate__tab-name")
+    .forEach(function (el) {
+        el.addEventListener("click", function (el){
+            switchTabs(this);
+        });
+    })
+
+function switchTabs(clickedTabButton) {
+    let indexOfClicked = tabButtons.indexOf(clickedTabButton);
+    tabButtons.forEach( el => {
+           el.classList.remove("active");
+    });
+    tabs.forEach( el => {
+           el.classList.remove("active");
+    });
+    tabButtons[indexOfClicked].classList.add("active");
+    tabs[indexOfClicked].classList.add("active");
+}
 const swiper = new Swiper(".rate__info-slider", {
     autoplay: true,
     loop: true,
@@ -5,5 +32,4 @@ const swiper = new Swiper(".rate__info-slider", {
         nextEl: ".rate__info-arrow-next",
         prevEl: ".rate__info-arrow-prev"
     }
-
 });
